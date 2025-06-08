5e1e4451774bb3e4521f3e008016dd8dbc4561bf
MiloosN5 <56539570+MiloosN5@users.noreply.github.com>
2025-06-08 13:02:00 +0200

    Update index.php
    formatting index.php

--- index.php
+++ index.php
@@ -1,14 +1,17 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <title>PHP BASICS</title>
     <link rel="stylesheet" href="assets/style.css">
 </head>
+
 <body>
     <h1>PHP Lessons</h1>
     <ul>
         <li><a href="php-lessons/data_types.php">Data Types</a></li>
     </ul>
 </body>
+
 </html>
